1b50ecfbfcfdb1f36965656851b2858690ea1bf2
hassan1235 <hassan12590@yahoo.com>
2018-07-22 01:44:57 +0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,24 @@
-<?php
-$host = "localhost";
-$username = "zex1_zex";
-$password = "mindkiller"; 
-$dbname = "zex1_zex";
-$connection = mysql_connect($host,$username,$password);
-if (!$connection){
-die('Could not connect: ' . mysql_error());
-}
-mysql_select_db($dbname) or die(mysql_error());
-mysql_query("SET NAMES utf8");
-?>
+<?php
+
+$host = "mysql";
+
+$username = "zex";
+
+$password = "mindkiller"; 
+
+$dbname = "zex";
+
+$connection = mysql_connect($host,$username,$password);
+
+if (!$connection){
+
+die('Could not connect: ' . mysql_error());
+
+}
+
+mysql_select_db($dbname) or die(mysql_error());
+
+mysql_query("SET NAMES utf8");
+
+?>
+
